9ee9b0c66734ca10de1e755089f231245939ab53
Anna Datsiuk <anna.datsiuk94@gmail.com>
2022-09-10 23:08:48 +0300

    Statistics done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
-import { Profile } from "./Profile/Profile";
-import user from '../user.json'
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data.json';
 
 export const App = () => {
   return (
-      <Profile username={user.username}
+    <>
+      <Profile
+        username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
-        stats={user.stats} />
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+export const Statistics = ({ stats, title }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statlist}>
+        {stats.map(stat => (
+        <li key={stat.id}  className={css.item} style={{backgroundColor:getRandomHexColor()}}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+        </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
